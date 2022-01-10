ae7fb0429ee5758b24ea3cadfe8c8ce8254c1d39
YINPRISIDENT <97436016+YINPRISIDENT@users.noreply.github.com>
2022-01-11 00:11:26 +0800

    pull

--- src/main/java/yyx/test/GitTest.java
+++ src/main/java/yyx/test/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hot-fix1");
         System.out.println("master1");
         System.out.println("push");
+        System.out.println("pull");
     }
 }
