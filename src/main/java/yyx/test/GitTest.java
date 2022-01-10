4d811995fd58bc2c210648a1e998775eb3e697f3
yyx <1078144521@qq.com>
2022-01-10 23:24:33 +0800

    first version

--- src/main/java/yyx/test/GitTest.java
+++ src/main/java/yyx/test/GitTest.java
@@ -3,5 +3,6 @@ package yyx.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git1");
+        System.out.println("hello Git2");
     }
 }
